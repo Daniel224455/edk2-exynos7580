ba3285507333dfdf8d2ce26fda70adeee0a228ae
Daniel224455 <danalexgro@gmail.com>
2025-09-23 22:57:15 +0200

    Improve boot.img creation. UEFI image turns out to only be 2mb instead of 15.

--- build.sh
+++ build.sh
@@ -4,15 +4,22 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 ./build_bootshim.sh
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p EXYNOS7580Pkg/Devices/a5xelte.dsc
-cat BootShim/BootShim.bin workspace/Build/EXYNOS7580Pkg/DEBUG_GCC5/FV/EXYNOS7580PKG_UEFI.fd > workspace/boot.img-zImage
-cd device_specific/aik
-./unpackimg_x64.sh boot.img
-rm split_img/boot.img-zImage
-cp ../../workspace/boot.img-zImage split_img/
-./repackimg_x64.sh boot.img
-mv image-new.img ../../workspace/boot.img
-./cleanup.sh
-cd ../../workspace/
+rm boot.img boot.tar
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -j$(nproc) -s -n 0 -a AARCH64 -t GCC5 -p EXYNOS7580Pkg/Devices/a5xelte.dsc||exit 1
+cat BootShim/BootShim.bin workspace/Build/EXYNOS7580Pkg/DEBUG_GCC5/FV/EXYNOS7580PKG_UEFI.fd > workspace/payload.bin||exit 1
+
+python3 ./device_specific/mkbootimg.py \
+	--kernel ./workspace/payload.bin \
+	--ramdisk ./device_specific/dummyramdisk \
+	--dtb "./device_specific/a5xe/a5xelte.dt" \
+	--base 0x10000000 \
+	--kernel_offset 0x00008000 \
+	--ramdisk_offset 0x01000000 \
+	--tags_offset 0x00000100 \
+	--pagesize 2048 \
+	--os_version 7.0.0 \
+	--os_patch_level "$(date '+%Y-%m')" \
+	--board "SRPOJ07A003KU" \
+	-o boot.img 
+
 tar -c boot.img -f boot.tar
-cd ..
